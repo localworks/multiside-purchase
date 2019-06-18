22cf969b6741ad2ccd0fed3e9a4ba7391c9f90e5
Kazuaki Takemoto <libkazz@gmail.com>
2019-06-18 17:53:48 +0900

    set test_framework

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module MultisidePurchase
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework false
+    end
   end
 end
